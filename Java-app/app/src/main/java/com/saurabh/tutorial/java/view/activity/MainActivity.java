682ef867434eacb9e5e2bc73e13b67d9e32fadb4
Khare, Saurabh <saurabh.khare@rakuten.com>
2018-07-07 16:59:25 +0900

    Homepage Activity added

--- Java-app/app/src/main/java/com/saurabh/tutorial/java/view/activity/MainActivity.java
+++ Java-app/app/src/main/java/com/saurabh/tutorial/java/view/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.saurabh.tutorial.java;
+package com.saurabh.tutorial.java.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.saurabh.tutorial.java.R;
 
 public class MainActivity extends AppCompatActivity {
 
